a6e425577d27c3bad44a4ebba4a1e605952c310d
$t@$h <sinatashakkori@gmail.com>
2023-10-03 22:05:10 -0400

    Update Matrix.kt

--- Matrix.kt
+++ Matrix.kt
@@ -10,19 +10,19 @@ import org.nd4j.linalg.ops.transforms.Transforms
 class Matrix {
   fun add(matrix1: INDArray, matrix2: INDArray): INDArray {
     require(matrix1.shape().contentEquals(matrix2.shape()))
-      { "Matrix dimensions must match for addition." }
+      { "Add: Matrix dimensions must match." }
     return matrix1.add(matrix2)
   }
 
   fun subtract(matrix1: INDArray, matrix2: INDArray): INDArray {
     require(matrix1.shape().contentEquals(matrix2.shape()))
-      { "Matrix dimensions must match for subtraction." }
+      { "Subtract: Matrices dimensions must match." }
     return matrix1.sub(matrix2)
   }
 
   fun multiply(matrix1: INDArray, matrix2: INDArray): INDArray {
     require(matrix1.size(1) == matrix2.size(0))
-      { "Matrix dimensions are incompatible for multiplication." }
+      { "Multiply: Matrices dimensions must be compatible." }
     return matrix1.mmul(matrix2)
   }
 
@@ -36,17 +36,17 @@ class Matrix {
 
   fun inverse(matrix: INDArray): INDArray {
     require(matrix.rows() == matrix.columns())
-      { "Matrix must be square for inversion." }
+      { "Inverse: Matrix must be square." }
     return InvertMatrix.invert(matrix, false)
   }
 
   fun determinant(matrix: INDArray): Double {
     require(matrix.rows() == matrix.columns())
-      { "Matrix must be square for determinant calculation." }
+      { "Determinant: Matrix must be square." }
     return Transforms.log(Nd4j.diag(matrix)).sumNumber().toDouble()
   }
 
-  fun identityMatrix(matrix: INDArray): INDArray {
+  fun identity(matrix: INDArray): INDArray {
     return Nd4j.eye(matrix.rows().toLong())
   }
 
@@ -62,14 +62,38 @@ class Matrix {
     return Nd4j.rand(rows, cols)
   }
 
-  fun createMatrix(data: Array<DoubleArray>): INDArray {
+  fun create(data: Array<DoubleArray>): INDArray {
     val matrixData = Array(data.size) { DoubleArray(data[0].size) }
     for (i in 0 until data.size) { matrixData[i] = data[i] }
     return Nd4j.create(matrixData)
   }
 
-  fun covarianceMatrix(data: INDArray): INDArray {
+  fun covariance(data: INDArray): INDArray {
     val centered = data.sub(data.mean(0))
     return (centered.transpose().mmul(centered)).div(data.rows().toDouble())
+  }
+
+  fun elementsAdd(matrix1: INDArray, matrix2: INDArray): INDArray {
+    require(matrix1.shape().contentEquals(matrix2.shape()))
+      { "elementsAdd: Matrices dimensions must match." }
+    return matrix1.addi(matrix2)
+  }
+
+  fun elementsSubtract(matrix1: INDArray, matrix2: INDArray): INDArray {
+    require(matrix1.shape().contentEquals(matrix2.shape()))
+      { "elementsSubtract: Matrices dimensions must match." }
+    return matrix1.subi(matrix2)
+  }
+
+  fun elementsMultiply(matrix1: INDArray, matrix2: INDArray): INDArray {
+    require(matrix1.shape().contentEquals(matrix2.shape()))
+      { "elementsMultiply: Matrix dimensions must match." }
+    return matrix1.muli(matrix2)
+  }
+
+  fun elementsDivide(matrix1: INDArray, matrix2: INDArray): INDArray {
+    require(matrix1.shape().contentEquals(matrix2.shape()))
+      { "elementsDivide: Matrix dimensions must match." }
+    return matrix1.divi(matrix2)
   }
 }
